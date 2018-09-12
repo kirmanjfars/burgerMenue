ae015defbf9ce7909ce69824cdddd4843a9bcb53
kirmanjfars <kirmanj.00806510@gmail.com>
2018-09-12 16:56:59 +0300

    burger menue

---
+++ js/java.js
@@ -0,0 +1,9 @@
+
+$('header button').on('click', function () {
+    $('.aside0').toggle('.aside1')
+})
+
+$('.fa-arrow-alt-circle-left').on('click', function () {
+    $('.aside0').toggle('.aside1')
+})
+
